0067093011b5fa81d485091e59654c82c42a085b
RFBS18 <fabriziobarrios22@gmail.com>
2023-08-23 02:03:30 -0500

    agregando vista de producto aceite y slider

--- app/src/main/java/com/fabridev/perusac/contenido.java
+++ app/src/main/java/com/fabridev/perusac/contenido.java
@@ -3,15 +3,33 @@ package com.fabridev.perusac;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class contenido extends AppCompatActivity {
-    private RecyclerView.Adapter adapter;
-    private RecyclerView listacategoria, listaproducto;
+    Button agregar2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contenido);
+        loadUI();
+        agregar2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirActivity(vista_aceite.class);
+            }
+        });
+    }
+
+    private void loadUI(){
+        agregar2 = findViewById(R.id.agregar2);
+    }
+
+    private void abrirActivity(Class nameActivity) {
+        Intent intent = new Intent(getApplicationContext(), nameActivity);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/fabridev/perusac/vista_aceite.java
@@ -0,0 +1,15 @@
+package com.fabridev.perusac;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class vista_aceite extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vista_aceite);
+    }
+}
